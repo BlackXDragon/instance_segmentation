e09a3bb1b2e5df9d9f2902fbc17a18ffd8336eb3
BlackXDragon <malolan98@gmail.com>
2025-02-12 21:32:45 +0000

    Proper logging

--- download_datasets.py
+++ download_datasets.py
@@ -8,7 +8,7 @@ import logging
 import tqdm
 import argparse
 
-logger = logging.getLogger("download_datasets")
+logger = logging.getLogger(__name__)
 
 def pre_download():
 	if not os.path.exists('data'):
@@ -17,18 +17,18 @@ def pre_download():
 
 def download_package_seg():
 	try:
-		logger.info('Downloading package-seg dataset')
-		data = None
-		with requests.get('https://ultralytics.com/assets/package-seg.zip', stream=True) as response:
-			response.raise_for_status()
-			for chunk in tqdm.tqdm(response.iter_content(chunk_size=1024), total=int(response.headers.get('content-length', 0)) // 1024, unit='kB'):
-				if chunk:
-					if data is None:
-						data = chunk
-					else:
-						data += chunk
-		with open('data/package_seg.zip', 'wb') as f:
-			f.write(data)
+		if not os.path.exists('data/package_seg.zip'):
+			logger.info('Downloading package-seg dataset')
+			with open('data/package_seg.zip', 'wb') as f:
+				pass
+			with requests.get('https://ultralytics.com/assets/package-seg.zip', stream=True) as response:
+				response.raise_for_status()
+				for chunk in tqdm.tqdm(response.iter_content(chunk_size=2**20), total=int(response.headers.get('content-length', 0)) // 2**20, unit='MB'):
+					if chunk:
+						with open('data/package_seg.zip', 'ab') as f:
+							f.write(chunk)
+		else:
+			logger.info('Package-seg dataset already exists')
 		logger.info('Extracting package-seg dataset')
 		with zipfile.ZipFile('data/package_seg.zip', 'r') as zip_ref:
 			zip_ref.extractall('data/package_seg')
@@ -44,8 +44,14 @@ def download_package_seg():
 
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Download datasets')
+
 	parser.add_argument('--package_seg', action='store_true', help='Download package-seg dataset', default=True)
+ 
+	parser.add_argument('-v', '--verbose', action='store_true', help='Enable verbose logging', default=False)
 	args = parser.parse_args()
+
+	if args.verbose:
+		logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 	pre_download()
 	if args.package_seg:
